542c504917b91364cd028b0edb078016eb201f9e
gautamrs224-r <gautamr.s155@gmail.com>
2026-08-12 17:08:12 +0530

    updated hello_world.py file

--- 01_Basics/01_hello_world.py
+++ 01_Basics/01_hello_world.py
@@ -2,3 +2,9 @@ print("Hello World!")
 
 print('Hi, My Name is Ravishankar Gautam aka Dev Gautam')
 print("I'm learning Python Programming language and I'm enjoying it a lot.")
+print("I want to become a Full Stack Python Developer and I will achieve it for sure. Becuase I'm a deligent and hardworking person and I never give up on my dreams.")
+
+# I think, I can use print() more than text means i can use numbers, variables, and expressions inside print() function. Let's see how it works.
+print("10")
+print(10)
+print("10 + 20 = ", 10 + 20)
